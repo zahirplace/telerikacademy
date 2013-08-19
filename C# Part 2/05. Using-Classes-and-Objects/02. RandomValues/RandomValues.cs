1245f94b45e5a21e05f0723695eb54eee9f739bc
zahirplace <akezhova@mail.bg>
2013-08-19 13:09:54 +0300

    Classes and Objects
    Write a program that generates and prints to the console 10 random
    values in the range [100, 200].

---
+++ C# Part 2/05. Using-Classes-and-Objects/02. RandomValues/RandomValues.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RandomValues
+{
+    class RandomValues
+    {
+        static void Main(string[] args)
+        {
+            /*
+            02. Write a program that generates and prints to the console 10 random values in the range [100, 200].
+            */
+
+            int numCount = 10;           
+            PrintRandomListOfNumbers(numCount);
+
+        }
+
+        static void PrintRandomListOfNumbers(int numCount)
+        {
+            Random random = new Random();
+            for (int number = 1; number <= numCount; number++)
+            {
+                int randomNumber = random.Next(101) + 100;
+                Console.WriteLine("{0} ", randomNumber);
+            }
+        }
+
+    }
+}
