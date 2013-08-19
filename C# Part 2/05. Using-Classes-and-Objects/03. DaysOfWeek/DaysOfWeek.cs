2296e181e4d8375848a1690d8671cb564b4e0b7c
zahirplace <akezhova@mail.bg>
2013-08-19 13:11:30 +0300

    Write a program that prints to the console which day of the week is today. Use System.DateTime.

---
+++ C# Part 2/05. Using-Classes-and-Objects/03. DaysOfWeek/DaysOfWeek.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DaysOfWeek
+{
+    class DaysOfWeek
+    {
+        static void Main(string[] args)
+        {
+            /*
+            03. Write a program that prints to the console which day of the week is today. Use System.DateTime.
+            */
+
+            Console.WriteLine("Today is {0}!", DateTime.Today.DayOfWeek.ToString());
+        }
+    }
+}
