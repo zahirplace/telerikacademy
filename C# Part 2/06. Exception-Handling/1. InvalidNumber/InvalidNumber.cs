1c7661e12b7c16715b1f5d2fe7ef2cca6da01d9a
zahirplace <akezhova@mail.bg>
2013-08-23 18:07:17 +0300

    Invalid number
    Write a program that reads an integer number and calculates and prints
    its square root. If the number is invalid or negative, print "Invalid
    number". In all cases finally print "Good bye". Use try-catch-finally.

---
+++ C# Part 2/06. Exception-Handling/1. InvalidNumber/InvalidNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InvalidNumber
+{
+    class InvalidNumber
+    {
+        static void Main(string[] args)
+        {
+            /*
+             1. Write a program that reads an integer number and calculates and prints its square root. 
+             If the number is invalid or negative, print "Invalid number". 
+             In all cases finally print "Good bye". 
+             Use try-catch-finally.
+            */
+
+
+            try
+            {
+                Console.Write("Enter a number: ");
+                string strNum = Console.ReadLine();
+                int intNum;
+                if (!int.TryParse(strNum, out intNum) || int.Parse(strNum) < 0)
+                {
+                    throw new System.ArgumentOutOfRangeException("Invalid number");
+                }
+                Console.WriteLine("The square of {0} is {1}.", strNum, Math.Sqrt(intNum));
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
